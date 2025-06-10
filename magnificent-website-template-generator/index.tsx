34c6a9d223779ef57bf80538f491ac936b583cd2
realmstoriches <robert.demotto@realmstoriches.xyz>
2025-06-10 07:11:35 -0400

    Update index.tsx

--- magnificent-website-template-generator/index.tsx
+++ magnificent-website-template-generator/index.tsx
@@ -1,11 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
+// Use a more specific ID for the app's root element to avoid conflicts
+const appRootElementId = 'magnificent-template-generator-app-root';
+const rootElement = document.getElementById(appRootElementId);
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error(`Could not find root element with ID '${appRootElementId}' to mount the React app. Make sure a div with this ID exists in your HTML.`);
 }
 
 const root = ReactDOM.createRoot(rootElement);
